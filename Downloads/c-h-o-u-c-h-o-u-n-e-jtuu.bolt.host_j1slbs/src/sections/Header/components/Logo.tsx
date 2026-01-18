c0c43478cdd632a36681d8a01850f240064ff69e
User <user@example.com>
2026-01-17 20:32:36 -0500

    Initial_commit

---
+++ Downloads/c-h-o-u-c-h-o-u-n-e-jtuu.bolt.host_j1slbs/src/sections/Header/components/Logo.tsx
@@ -0,0 +1,13 @@
+export const Logo = () => {
+  return (
+    <div className="box-border caret-transparent flex justify-center">
+      <a href="/" className="box-border caret-transparent block w-full">
+        <img
+          src="https://c.animaapp.com/mkj1il3o8LXpDA/assets/logo-DWYo-pdM.png"
+          alt="Les Confidences d'Amandine"
+          className="box-border caret-transparent max-h-96 max-w-full object-contain opacity-90 w-full"
+        />
+      </a>
+    </div>
+  );
+};
